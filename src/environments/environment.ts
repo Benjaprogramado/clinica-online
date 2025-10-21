2698eb95290f6abbc5c96faf86a3cac8f42630f6
Benjamin <benjaschell@gmail.com>
2025-10-21 02:01:48 -0300

    estilos + firebase OK

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+    production: true, // ← Cambiado a true para producción
+    firebaseConfig: {
+        apiKey: "AIzaSyCx2gMBUuU8D8eHGDAhE_w682C-B1pjyn8",
+        authDomain: "clinica-online-56d43.firebaseapp.com",
+        projectId: "clinica-online-56d43",
+        storageBucket: "clinica-online-56d43.firebasestorage.app",
+        messagingSenderId: "436008566131",
+        appId: "1:436008566131:web:518785dc5a9a5876375906"
+    }
+  };
